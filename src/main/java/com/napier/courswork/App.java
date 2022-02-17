dbfc0d8bcc9da0f660d61d187d1ffba3970387f4
“Ali <40522091@live.napier.ac.uk>
2022-02-17 13:47:08 -0500

    Update the Database

--- src/main/java/com/napier/courswork/App.java
+++ src/main/java/com/napier/courswork/App.java
@@ -4,9 +4,134 @@ import java.sql.*;
 
 public class App
 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        // Create new Application
+        App a = new App();
 
-        System.out.println("Test code");
+        // Connect to database
+        a.connect();
+        // Get Country
+        Country ctry = a.getCountry("JAM");
+        // Display results
+        a.displayCountry(ctry);
+
+        // Disconnect from database
+        a.disconnect();
     }
 
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
+        try
+        {
+            // Load Database driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("Could not load SQL driver");
+            System.exit(-1);
+        }
+
+        int retries = 10;
+        for (int i = 0; i < retries; ++i)
+        {
+            System.out.println("Connecting to database...");
+            try
+            {
+                // Wait a bit for db to start
+                Thread.sleep(30000);
+                // Connect to database
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
+                System.out.println("Successfully connected");
+                break;
+            }
+            catch (SQLException sqle)
+            {
+                System.out.println("Failed to connect to database attempt " + i);
+                System.out.println(sqle.getMessage());
+            }
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
+        }
+    }
+
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
+        if (con != null)
+        {
+            try
+            {
+                // Close connection
+                con.close();
+            }
+            catch (Exception e)
+            {
+                System.out.println("Error closing connection to database");
+            }
+        }
+    }
+
+
+    public Country getCountry(String Code)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            System.out.println("Country details");
+            String strSelect =
+                    "SELECT code, name, continent "
+                            + "FROM country "
+                            + "WHERE code = '" + Code + "'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new countryif valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                System.out.println("Display details");
+                Country ctry = new Country();
+                ctry.Code = rset.getString("code");
+                ctry.Name = rset.getString("name");
+                ctry.Continent = rset.getString("continent");
+                return ctry;
+            }
+            else {
+                System.out.println("No records");
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+    public void displayCountry(Country ctry)
+    {
+        if (ctry != null)
+        {
+            System.out.println(
+                    "Country Code: " +  ctry.Code + "\n"
+                            + "Country Name: " + ctry.Name + "\n"
+                            + "Continent: " + ctry.Continent + "\n");
+        }
+    }
 }
