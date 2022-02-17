84a61b332eb8e0f650762f725954a771bc5c6169
stephensonc-jm <40520885@live.napier.ac.uk>
2022-02-16 22:19:18 -0500

    Display country details

--- src/main/java/com/napier/courswork/App.java
+++ src/main/java/com/napier/courswork/App.java
@@ -6,6 +6,30 @@ public class App
 {
     public static void main(String[] args)
     {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+        // Get Country
+        Country ctry = a.getCountry("JAM");
+        // Display results
+        a.displayCountry(ctry);
+
+        // Disconnect from database
+        a.disconnect();
+    }
+
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
+    {
         try
         {
             // Load Database driver
@@ -17,9 +41,7 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
@@ -30,9 +52,6 @@ public class App
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-                Thread.sleep(10000);
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
@@ -45,7 +64,13 @@ public class App
                 System.out.println("Thread interrupted? Should not happen.");
             }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
@@ -57,6 +82,56 @@ public class App
             {
                 System.out.println("Error closing connection to database");
             }
+        }
+    }
+
+
+    public Country getCountry(String Code)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            System.out.println("Country details");
+            String strSelect =
+                    "SELECT code, name, continent "
+                            + "FROM country "
+                            + "WHERE code = '" + Code + "'";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new countryif valid.
+            // Check one is returned
+            if (rset.next())
+            {
+                System.out.println("Display details");
+                Country ctry = new Country();
+                ctry.Code = rset.getString("code");
+                ctry.Name = rset.getString("name");
+                ctry.Continent = rset.getString("continent");
+                return ctry;
+            }
+            else {
+                System.out.println("No records");
+                return null;
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get country details");
+            return null;
+        }
+    }
+
+    public void displayCountry(Country ctry)
+    {
+        if (ctry != null)
+        {
+            System.out.println(
+                    "Country Code: " +  ctry.Code + "\n"
+                            + "Country Name: " + ctry.Name + "\n"
+                            + "Continent: " + ctry.Continent + "\n");
         }
     }
 }
