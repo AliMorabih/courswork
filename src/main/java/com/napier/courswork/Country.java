f5b99f7e7e876fe347154c2502bd67b3d69d5578
“Ali <40522091@live.napier.ac.uk>
2022-02-20 21:00:30 -0500

    Merge branch 'feature/countryreport'
    # Conflicts:
    #	README.md

---
+++ src/main/java/com/napier/courswork/Country.java
@@ -0,0 +1,13 @@
+package com.napier.courswork;
+
+public class Country {
+
+    public String Code;
+    public String Name;
+    public String Continent;
+    public String Region;
+    public int  Population;
+    public int Capital;
+
+
+}
