3a75fe810cbde59e940cf9fbd1bef5f9de5eb340
stephensonc-jm <40520885@live.napier.ac.uk>
2022-02-22 16:26:27 -0500

    Added the following reports: •	All the cities in the world organised by largest population to smallest. •	All the cities in a continent organised by largest population to smallest.

--- src/main/java/com/napier/courswork/CityPopulationDAL.java
+++ src/main/java/com/napier/courswork/CityPopulationDAL.java
@@ -14,7 +14,7 @@ public class CityPopulationDAL {
             System.out.println("All the cities in the world organised by largest population to smallest \n");
 
             String strSelect =
-                    "SELECT id, city.name, country.name,country.continent,country.region, city.Population "
+                    "SELECT id, city.name, country.name,city.district,city.Population "
                             + "FROM country, city "
                             + " WHERE city.countrycode = country.code "
                             + "ORDER BY city.Population DESC" ;
@@ -30,8 +30,7 @@ public class CityPopulationDAL {
                 CityPopulation city = new CityPopulation();
                 city.CityName = rset.getString("city.name");
                 city.CountryName = rset.getString("country.name");
-                city.Continent = rset.getString("country.continent");
-                city.Region = rset.getString("country.Region");
+                city.District = rset.getString("city.district");
                 city.Population = rset.getInt("city.population");
                 citypop.add(city);
             }
@@ -54,7 +53,7 @@ public class CityPopulationDAL {
             System.out.println("All the cities in a continent organised by largest population to smallest \n");
 
             String strSelect =
-                    "SELECT id, city.name, country.name,country.continent,country.region, city.Population "
+                    "SELECT id, city.name, country.name,city.district,city.Population "
                             + " FROM country, city "
                             + " WHERE city.countrycode = country.code "
                             + " and country.continent = 'Asia'"
@@ -71,8 +70,7 @@ public class CityPopulationDAL {
                 CityPopulation city = new CityPopulation();
                 city.CityName = rset.getString("city.name");
                 city.CountryName = rset.getString("country.name");
-                city.Continent = rset.getString("country.continent");
-                city.Region = rset.getString("country.Region");
+                city.District = rset.getString("city.District");
                 city.Population = rset.getInt("city.population");
                 citypop.add(city);
             }
@@ -89,13 +87,13 @@ public class CityPopulationDAL {
     public void printCityPopulation(ArrayList<CityPopulation> citypopulation)
     {
         // Print header
-        System.out.println(String.format("%-30s %-30s %-20s %-30s %-20s ", "CityName", "Country Name", "Continent" , "Region", "Population"));
+        System.out.println(String.format("%-30s %-30s %-20s %-20s ", "CityName", "Country Name", "District" , "Population"));
         // Loop over all employees in the list
         for (CityPopulation city : citypopulation)
         {
             String emp_string =
-                    String.format("%-30s %-30s %-20s %-30s %-20s",
-                            city.CityName, city.CountryName, city.Continent,  city.Region, city.Population);
+                    String.format("%-30s %-30s %-20s %-20s",
+                            city.CityName, city.CountryName, city.District, city.Population);
             System.out.println(emp_string);
         }
     }
