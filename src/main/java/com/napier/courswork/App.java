089f74e039346d955f518e286bee50fa877ace75
“Ali <40522091@live.napier.ac.uk>
2022-04-26 20:23:06 -0400

    Deployment

--- src/main/java/com/napier/courswork/App.java
+++ src/main/java/com/napier/courswork/App.java
@@ -108,7 +108,7 @@ public class App
         System.out.println("****Display the top four  populated cities in the world******");
         System.out.println("*************************************************************");
         ArrayList<City> CitiW = CIT.getFourPopulatedCityWorld(a.con);
-        CIT.printPopulatedCityWorld(CitiW, "getFourPopulatedCityWorld.md");
+        CIT.printPopulatedCityWorld(CitiW, "PopulatedCityWorld.md");
 
 
 
